b1c7adcbf44fff8b27569d81c4d402c5f1e5f512
Daniel Arroyo <37883725+danilockthar@users.noreply.github.com>
2021-05-17 12:06:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,7 @@ module.exports = {
         config.devtool = isProd ? config.devtool : 'eval-source-map';
         return config;
       },
-      faunaDbAdminKey: 'fnAD1d55EtACAPq7lsNJKHFTdBFVR6ew6PZ6Esmm',
-      faunaDbSecret: 'fnAD1eDZSxACEtTdveFm5qyxjg-yeEkpA1cMvN_7',
-      faunaDbGraphQlEndpoint: 'https://graphql.fauna.com/graphql',
-      checkoutSecret : 'Madrid*2019*',
-      nameProject: "El Tío Caracoles",
-      toHome: isProd ? 'https://eltiocaracoles.com/' : 'http://localhost:3000/',
-      profileInfo: isProd ? 'https://eltiocaracoles.com/api/profile' : 'http://localhost:3000/api/profile',
+      
     },
   }
   
